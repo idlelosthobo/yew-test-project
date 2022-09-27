bfb5af34c74ce0ba1c20994bf0b551039d3785fb
Nathan Johnson <drugen@gmail.com>
2022-09-26 22:23:41 -0600

    WOOOOO

--- src/app/game/puzzle.rs
+++ src/app/game/puzzle.rs
@@ -9,13 +9,10 @@ struct Puzzle {
 
 #[function_component(PuzzleBoard)]
 pub fn puzzle_board() -> Html {
-    let tacos = vec![
-        cell::CellProps {number: 1},
-        cell::CellProps {number: 2},
-        cell::CellProps {number: 3},
-        cell::CellProps {number: 4},
-        cell::CellProps {number: 5},
-        ];
+    let mut tacos = vec![];
+    for i in 0..18 {
+        tacos.push(cell::CellProps {number: i});
+    }
     let videos = tacos.iter().map(|cell_data| html! {
         <cell::Cell number={cell_data.number} />
     }).collect::<Html>();
